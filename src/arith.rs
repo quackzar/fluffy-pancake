d9458f47fb96410c4b5870167138172e908c1a02
Mikkel Madsen <mikkel.wienberg@gmail.com>
2022-02-10 14:07:30 +0100

    Revert "More modulo"
    This reverts commit 412be2e219dba0da7e0da3e68d19cd3d489b4324.

--- src/arith.rs
+++ src/arith.rs
@@ -286,14 +286,14 @@ fn garble(circuit: &NewCircuit, k: u64) -> (HashMap<usize,Vec<Wire>>, Encoding, 
                 let tau = lsb(wires[a].values[0]);
 
                 let h = hash(i, 0, &(&wires[a] - &(delta_m * tau)));
-                let hw = wire_with(delta_n.domain, delta_n.lambda, h % delta_n.domain);
+                let hw = wire_with(delta_n.domain, delta_n.lambda, h);
                 let mut w = &hw + &(delta_n * phi(additive_inverse(tau, domain)));
                 wire_negate(&mut w);
 
                 let mut g = Vec::with_capacity(gate.domain as usize);
                 for x in 0..domain {
                     let h = hash(i, 0, &(&wires[a] + &(delta_m * x)));
-                    let hw = wire_with(delta_n.domain, delta_n.lambda, h % delta_n.domain);
+                    let hw = wire_with(delta_n.domain, delta_n.lambda, h);
                     let wx = &(&hw + &w) + &(delta_n * phi(x));
 
                     g.push(wx);
