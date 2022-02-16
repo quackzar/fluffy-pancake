10ec4409dcc65da89cf60a6fa9a3f94eb58b922a
Mikkel Madsen <mikkel.wienberg@gmail.com>
2022-02-16 18:41:40 +0100

    implement OT test

--- src/ot.rs
+++ src/ot.rs
@@ -1,6 +1,7 @@
 // Library for fast OT.
 // use curve25519_dalek::edwards;
 use curve25519_dalek::scalar::Scalar;
+use curve25519_dalek::edwards::EdwardsPoint;
 // use curve25519_dalek::edwards;
 use aes_gcm::{Aes256Gcm, Key, Nonce};
 use aes_gcm::aead::{Aead, NewAead};
@@ -24,6 +25,55 @@ mod tests {
 
     use super::*;
 
+    #[allow(non_snake_case)]
+    #[test]
+    fn oblivious_transfer() {
+        let mut rng = ChaCha12Rng::from_entropy();
+        let m0 = vec![0; 8];
+        let m1 = vec![1; 8];
+        let c = 0; // Choice
+
+        let g = Scalar::random(&mut rng);
+        let a = Scalar::random(&mut rng);
+        let b = Scalar::random(&mut rng);
+        let A = g * a; // Send A to receiver.
+        let B : Scalar;
+        if c == 0 {
+            B = g * b;
+        } else {
+            B = A + (g * b);
+        } // send B back to sender.
+        let mut hasher = Sha256::new();
+        hasher.update( (B * a).as_bytes() );
+        let k0 = hasher.finalize();
+        let mut hasher = Sha256::new();
+        hasher.update( ((B - A) * a).as_bytes() );
+        let k1 = hasher.finalize();
+
+        let cipher = Aes256Gcm::new(Key::from_slice(&k0));
+        let nonce = Nonce::from_slice(b"unique nonce");
+        let e0 = cipher.encrypt(nonce, m0.as_slice()).unwrap();
+        let cipher = Aes256Gcm::new(Key::from_slice(&k1));
+        let nonce = Nonce::from_slice(b"unique nonce");
+        let e1 = cipher.encrypt(nonce, m1.as_slice()).unwrap();
+
+        // send the e0, e1 to receiver.
+        let mut hasher = Sha256::new();
+        hasher.update((A * b).as_bytes());
+        let kR = hasher.finalize();
+
+        let key = Key::from_slice(&kR);
+        let cipher = Aes256Gcm::new(key);
+        let nonce = Nonce::from_slice(b"unique nonce"); // HACK: hardcoded, has to be 96-bit.
+        let m_c = if c == 0 {
+            cipher.decrypt(nonce, e0.as_ref()).unwrap()
+        } else {
+            cipher.decrypt(nonce, e1.as_ref()).unwrap()
+        };
+        assert_eq!(m_c, m0);
+    }
+
+    #[allow(non_snake_case)]
     #[test]
     fn diffie_hellman() {
         let mut rng = ChaCha12Rng::from_entropy();
