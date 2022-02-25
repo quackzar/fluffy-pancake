6b04a5dbc19123f5e0c61b8b444e76b920aefa04
Frederik Munk Madsen <frederik@os-madsen.dk>
2022-02-25 11:08:51 +0100

    WIP 1-to-n OT

--- src/ot.rs
+++ src/ot.rs
@@ -13,6 +13,7 @@ use rand_chacha::ChaCha12Rng;
 
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
+use crate::hash;
 
 // Common
 pub type CiphertextPair = [Vec<u8>; 2];
@@ -186,6 +187,130 @@ impl ObliviousReceiver<RetrievingPayload> {
             messages.push(m);
         }
         messages.try_into().unwrap()
+    }
+}
+
+// 1-to-n extensions for OT :D
+// https://dl.acm.org/doi/pdf/10.1145/301250.301312
+fn xor_bytes(left: &mut [u8; 32], right: &[u8; 32]) {
+    for i in 0..32 {
+        left[i] ^= right[i];
+    }
+}
+
+fn fk(key: &[u8; 32], choice: u16) -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    hasher.update(choice.to_be_bytes());
+    hasher.update(key);
+    let result = hasher.finalize();
+    return <[u8; 32]>::try_from(result.as_ref()).unwrap();
+}
+
+// How to 1-to-n:
+// 1. Initiate
+// 2. Challenge respond
+// 3. Choose
+// 4. Finish
+
+fn one_to_n_initiate(domain: u16, x: &Vec<[u8; 32]>)
+{
+    const wire_bytes: usize = 32;
+
+    // 1. B: Prepare random keys
+    let l = x.len();
+    let mut rng = ChaCha12Rng::from_entropy();
+    let mut keys: Vec<[[u8; wire_bytes]; 2]> = Vec::with_capacity(l);
+    for i in 0..l {
+        let left = Scalar::random(&mut rng).to_bytes();
+        let right = Scalar::random(&mut rng).to_bytes();
+        keys.push([left, right]);
+    }
+
+    let domain_max = 1 << domain; // 2^domain
+    let mut y = Vec::with_capacity(domain_max);
+    for i in 0..domain_max {
+        let mut value = x[i];
+        for j in 0..domain {
+            let bit = 1; // TODO: Grab the j'th bit from i
+            let hash = fk(&keys[j as usize][bit as usize], j);
+            xor_bytes(&mut value, &hash);
+        }
+
+        y.push(value);
+    }
+
+    // 2. Initiate 1-out-of-2 OTs by sending challenges
+    let mut senders = Vec::with_capacity(l);
+    let mut challenges = Vec::with_capacity(l);
+    for i in 0..l {
+        let m0 = keys[i as usize][0].to_vec();
+        let m1 = keys[i as usize][1].to_vec();
+
+        let message = Message::new(&[[m0, m1]]);
+        let sender = ObliviousSender::new(&message);
+
+        senders.push(sender);
+        challenges.push(sender.public());
+    }
+
+    // TODO: Send challenges to other party
+}
+
+fn one_to_n_challenge_respond(domain: u16, choice: u16) {
+    let l = 1 << domain;
+    let mut receivers = Vec::with_capacity(l);
+    for i in 0..l {
+        // TODO: Grab the corresponding bit from choice!
+        let receiver = ObliviousReceiver::new(&[true]);
+        receivers.push(receiver);
+    }
+
+    // TODO: Actually receive the challenges
+    // NOTE: This assume we get them in order!
+    let challenges: Vec<Public> = Vec::new();
+    debug_assert!(receivers.len() == challenges.len());
+    debug_assert!(challenges.len() == l);
+    for i in 0..l {
+        receivers[i].accept(&challenges[i]);
+    }
+
+    // TODO: Send responses to other party
+}
+
+fn one_to_n_choose(domain: u16) {
+    // TODO: Actually receive responses from other party
+    let responses: Vec<Public> = Vec::new();
+    let mut payloads: Vec<Payload> = Vec::with_capacity(l);
+    debug_assert!(challenges.len() == responses.len());
+    debug_assert!(responses.len() == l);
+    for i in 0..l {
+        let payload = senders[i].accept(&responses[i]);
+        payloads.push(payload);
+    }
+
+    // 3. B: Send the strings Y to A
+    // TODO: Send payloads to other party
+    // TODO: Send Ys to other party
+}
+
+fn one_to_n_finish(domain: u16, choice: u16, receivers: &Vec<ObliviousReceiver<S>>) {
+    let l = 1 << domain;
+
+    // TODO: Receive payloads
+    // TODO: Receive Ys
+    let payloads: Vec<Payload> = Vec::new();
+    let y: Vec<[u8; 32]> = Vec::new();
+
+    // Convert payloads to keys
+    let mut keys: Vec<[u8; 32]> = Vec::new();
+    for i in 0..l {
+        let message = receivers[i].retrieve(payloads[i]);
+    }
+
+    // Reconstruct X_I
+    let mut x = y[choice];
+    for i in 0..l {
+        xor_bytes(&mut x, &fk(&keys[i], choice));
     }
 }
 
