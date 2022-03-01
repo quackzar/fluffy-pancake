43769233da9aa5c5abb9d9e982682c4d8fdcc572
Mikkel Madsen <mikkel.wienberg@gmail.com>
2022-03-01 16:57:02 +0100

    Update benchmarks

--- benches/garble.rs
+++ benches/garble.rs
@@ -3,19 +3,24 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use magic_pake::fpake::build_circuit;
 use magic_pake::garble::{encode, evaluate, garble};
 
-fn bench_garble(c: &mut Criterion) {
+fn bench_garble_eval16(c: &mut Criterion) {
     const BITS: usize = 16;
     let circuit = build_circuit(BITS, 8);
-    c.bench_function("Garble", |b| b.iter(|| garble(&circuit)));
-}
-
-fn bench_eval(c: &mut Criterion) {
-    const BITS: usize = 16;
-    let circuit = build_circuit(BITS, 8);
+    c.bench_function("Garble 16bit", |b| b.iter(|| garble(&circuit)));
     let (gc, e, _) = garble(&circuit);
     let x = encode(&e, &vec![1; 2 * BITS]);
-    c.bench_function("Eval garbled", |b| b.iter(|| evaluate(&gc, &x)));
+    c.bench_function("Eval garbled 16bit", |b| b.iter(|| evaluate(&gc, &x)));
 }
 
-criterion_group!(benches, bench_garble, bench_eval);
+
+fn bench_garble_eval128(c: &mut Criterion) {
+    const BITS: usize = 128;
+    let circuit = build_circuit(BITS, 8);
+    c.bench_function("Garble 128bit", |b| b.iter(|| garble(&circuit)));
+    let (gc, e, _) = garble(&circuit);
+    let x = encode(&e, &vec![1; 2 * BITS]);
+    c.bench_function("Eval garbled 128bit", |b| b.iter(|| evaluate(&gc, &x)));
+}
+
+criterion_group!(benches, bench_garble_eval16, bench_garble_eval128);
 criterion_main!(benches);
