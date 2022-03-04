494690acbbf0ed3b625d9ddeeb4e4cd2929efcc6
Mikkel Madsen <mikkel.wienberg@gmail.com>
2022-03-04 11:32:41 +0100

    32k OT test

--- benches/ot.rs
+++ benches/ot.rs
@@ -129,6 +129,23 @@ fn bench_1_of_n_ot(c: &mut Criterion) {
         let payload = one_to_n_create_payloads(&sender, &response);
         let _output = one_to_n_choose(domain, choice, &receiver, &payload, &y);
     }));
+
+
+
+    let n = 1 << 15;
+    let domain = log2(n);
+    let mut messages = Vec::with_capacity(n as usize);
+    for i in 0..n {
+        messages.push(i.to_be_bytes().to_vec());
+    }
+    let choice = 4;
+
+    c.bench_function("OT 1-out-of-32768", |b| b.iter(|| {
+        let (sender, challenge, y) = one_to_n_challenge_create(domain, &messages);
+        let (receiver, response) = one_to_n_challenge_respond(domain, choice, &challenge);
+        let payload = one_to_n_create_payloads(&sender, &response);
+        let _output = one_to_n_choose(domain, choice, &receiver, &payload, &y);
+    }));
 }
 
 
