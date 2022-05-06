d6155190df6e79fb162f7e9a8c0e3ea9a45613ae
Frederik Munk Madsen <frederik@os-madsen.dk>
2022-05-06 13:48:30 +0200

    Updated instrumentation colors

--- src/instrument.rs
+++ src/instrument.rs
@@ -1,11 +1,18 @@
 #[cfg(target_os = "windows")]
 use superluminal_perf::*;
 
+// IO: Red
 pub const E_RECV_COLOR: u32 = 0xFF6D6DFF;
-pub const E_SEND_COLOR: u32 = 0x6DFF6DFF;
+pub const E_SEND_COLOR: u32 = 0xFF6D6DFF;
+
+// COMPUTATION: Yellow
 pub const E_COMP_COLOR: u32 = 0xFFFF6DFF;
-pub const E_PROT_COLOR: u32 = 0x6D6DFFFF;
-pub const E_FUNC_COLOR: u32 = 0xFF6DFFFF;
+
+// FUNCTION: Blue
+pub const E_FUNC_COLOR: u32 = 0x1f1f91FF;
+
+// PROTOCOL: Green
+pub const E_PROT_COLOR: u32 = 0x6DFF6DFF;
 
 #[inline(always)]
 #[cfg(target_os = "windows")]
